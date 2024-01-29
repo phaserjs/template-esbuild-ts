a0555b5f41168ee17dc6dcac6d3951e5e05ca6dd
Francisco Pereira Alvarado <gammafp@gmail.com>
2024-01-29 15:46:12 +0100

    init

---
+++ esbuild/build.prod.js
@@ -0,0 +1,47 @@
+import { build } from 'esbuild';
+import clean from 'esbuild-plugin-clean';
+import copy from 'esbuild-plugin-copy';
+import inlineImage from 'esbuild-plugin-inline-image';
+
+let msgPhaser = {
+    name: 'msg-phaser',
+    setup(build) {
+        build.onEnd(() => {
+            const line = "---------------------------------------------------------";
+            const msg = `❤️❤️❤️ Tell us about your game! - games@phaser.io ❤️❤️❤️`;
+            process.stdout.write(`${line}\n${msg}\n${line}\n`);
+
+            process.stdout.write(`✨ Done ✨\n`);
+        });
+    },
+}
+
+const builder = async () => {
+    await build({
+        entryPoints: ['./src/main.ts'],
+        bundle: true,
+        minify: true,
+        sourcemap: false,
+        target: ['chrome58', 'firefox57', 'safari11', 'edge16'],
+        outfile: './dist/bundle.min.js',
+        plugins: [
+            clean({
+                patterns: ['./dist/*', './public/bundle.min.js'],
+            }),
+            inlineImage({
+                namespace: 'assets',
+            }),
+            copy({
+                assets: [
+                    { from: './public/index.html', to: './' },
+                    { from: './public/style.css', to: './' },
+                    { from: './public/favicon.ico', to: './' },
+                    { from: './public/favicon.png', to: './' },
+                    { from: './public/assets/**/*', to: './assets/' }
+                ],
+            }),
+            msgPhaser
+        ]
+    });
+};
+builder();
---
+++ esbuild/dev.server.js
@@ -0,0 +1,13 @@
+import esbuildServe from "esbuild-serve";
+import inlineImage from "esbuild-plugin-inline-image";
+
+esbuildServe(
+    {
+        logLevel: "info",
+        entryPoints: ["src/main.ts"],
+        bundle: true,
+        outfile: "public/bundle.min.js",
+        plugins: [ inlineImage() ]
+    },
+    { root: "public", port: 8080 },
+);
